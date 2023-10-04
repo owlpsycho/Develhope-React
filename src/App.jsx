c93d9cb8ef75b2608428d42047230a3f95459971
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-04 19:17:20 +0200

    risoluzione esercizio 13

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { Hello } from './Hello'
-import { Message } from './Message'
 
 function App() {
 
   return (
     <>
       <Hello />
-      <Message />
+      <Hello />
     </>
   )
 }
