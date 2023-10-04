52732b8029ea674cdb35231cc61f2540e113afdc
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-04 14:04:08 +0200

    risoluzione esercizio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Hello } from './Hello'
 
 function App() {
 
+
   return (
     <>
-      
+      <Hello name="Denis" />
     </>
   )
 }
---
+++ src/Hello.jsx
@@ -0,0 +1,3 @@
+export function Hello ({ name }) {
+    return <h1>Hello, {name}!</h1>
+}
