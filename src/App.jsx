cc9a253e4479a2f32d01e86f4c061f2a46b805c1
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-04 14:21:49 +0200

    risoluzione esercizio 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Hello } from './Hello'
 
 function App() {
 
   return (
     <>
-      
+      <Hello />
+      //darà come risultato solo "Hello, !" se non passo la variabile name//
     </>
   )
 }
