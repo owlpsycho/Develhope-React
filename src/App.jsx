5919aedb468f6ac81f677b0229f3afc38a0292a8
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-04 19:05:45 +0200

    risoluzione esercizio 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { Hello } from './Hello'
 
 function App() {
 
   return (
     <>
-      <h1>FATTO!</h1>
+      <Hello />
     </>
   )
 }
---
+++ src/Hello.jsx
@@ -0,0 +1,8 @@
+export function Hello () {
+    return 
+        <div>
+           <h2>Hello, World!</h2>
+           <p>What a beautiful day!</p> 
+        </div>
+        
+}
