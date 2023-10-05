75aedf1642f69e52192390ca06f3db0b292b0310
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-05 11:49:03 +0200

    risoluzione esercizio 21

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { Welcome } from './Welcome'
 
 function App() {
 
   return (
     <>
-      
+      <Welcome name="Denis" age={17} />
     </>
   )
 }
