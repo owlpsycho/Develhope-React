a86f4d27a46d17cb7a684e8079d3f58c0818a7ad
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-05 11:50:09 +0200

    risoluzione esercizio 22

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -2,7 +2,7 @@ export function Welcome({ name, age }) {
     return (
         <>
             <h1>Welcome, {name}</h1>
-            {age>=18 && <p>You are {age} years old!</p>}
+            {age && <p>You are {age} years old!</p>}
         </>
     )
 }
