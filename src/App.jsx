c1e8d270645341cc86dd0b42038ede496712fc4d
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-06 21:09:48 +0200

    risoluzione esercizio 35

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { Clock } from './Clock'
 
 function App() {
 
   return (
     <>
-      
+      <Clock />
     </>
   )
 }
---
+++ src/Clock.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+export const Clock = () => {
+  const [time, setTime] = useState(currentTime());
+
+  function currentTime() {
+    const time = new Date().toLocaleTimeString();
+    return time;
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(currentTime());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <h2>{time}</h2>;
+};
