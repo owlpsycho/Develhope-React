7d41d2e00b9e618b90edbb12894676934c75b1e8
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-06 19:20:37 +0200

    risoluzione esercizio 32

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react"
+import { CounterDisplay } from "./CounterDisplay"
 
 export function Counter(){
     const [count, setCount] = useState(0)
     function handleClick (){
         setCount(count + 1)
-        // usiamo la funzione dentro setCount se dobbiamo fare più operazioni su count, 
-        //sennò utilizziamo un valore immediato se dobbiamo eseguire una semplice operazione
     }
 
     return (
         <>
-            <h2>{count}</h2>
+            <CounterDisplay value={count} />
             <button onClick={handleClick}>Click Me!</button>
         </>
     )
---
+++ src/CounterDisplay.jsx
@@ -0,0 +1,5 @@
+export function CounterDisplay({ value }){
+
+
+    return <h2>{value}</h2>
+}
