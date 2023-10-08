fa9d38b19fb328cc8e47451f30a46dd137f84f2f
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-08 19:03:49 +0200

    risoluzione esercizio 38

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,13 +1,17 @@
 export const MouseClicker = ({ name }) => {
     const handleClicker = (event) => {
-        console.log(event.target.name)
+        console.log(event.currentTarget.name)
     }
 
+    const handleImgClicker = (event) => {
+        console.log(event.currentTarget.src)
+    }
 
+        //vengono eseguiti insieme a causa dell'effetto bubble di html dato che l'immagine si trova all'interno del bottone
 
     return (
         <>
-            <button name={name} onClick={handleClicker}>Click Me!</button>
+            <button name={name} onClick={handleClicker}><img src="./assets/react.svg" onClick={handleImgClicker}/>Click Me!</button>
         </>
     )
 }
