506fcc6df21b49bdf388f5b27c6053542811a87d
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-08 18:42:06 +0200

    risoluzione esercizio 36

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { MouseClicker } from './MouseClicker'
 
 function App() {
 
   return (
     <>
-      
+      <MouseClicker name="ONE"/>
     </>
   )
 }
---
+++ src/MouseClicker.jsx
@@ -0,0 +1,13 @@
+export const MouseClicker = ({ name }) => {
+    const handleClicker = (event) => {
+        console.log(event.target.name)
+    }
+
+
+
+    return (
+        <>
+            <button name={name} onClick={handleClicker}>Click Me!</button>
+        </>
+    )
+}
