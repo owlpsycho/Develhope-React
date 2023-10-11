97c975c3ce1dcecbd0772ad28426e484c097f1c5
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-11 19:08:02 +0200

    risoluzione esercizio 41

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { InteractiveWelcome } from './InteractiveWelcome'
 
 function App() {
 
   return (
     <>
-      
+      <InteractiveWelcome />
     </>
   )
 }
---
+++ src/InteractiveWelcome.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Welcome } from "../src/Welcome";
+
+export function InteractiveWelcome() {
+  const [name, setName] = useState("");
+
+  function handleChangeInput(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <>
+      <form>
+        <input onChange={handleChangeInput} value={name} />
+      </form>
+      <Welcome name={name} />
+    </>
+  )
+}
