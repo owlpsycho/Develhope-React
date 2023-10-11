1729852deb2816a73236d1dc43ef6da0cee3bb79
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-11 20:18:18 +0200

    risoluzione esercizio 43

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-export function Login() {
+export function Login({ onLogin }) {
   const [data, setData] = useState({
     username: '',
     password: '',
@@ -11,23 +11,28 @@ export function Login() {
     const name = event.target.name;
     const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
 
-
     setData(data => {
       return {
         ...data, 
         [name]: value
-        
       }
-    })
-}
+    });
+  }
+
+  const isButtonDisabled = data.username === '' || data.password === '';
+
+  const handleLogin = () => {
+    onLogin(data);
+  };
 
   return (
     <>
-    <form>
-      <input type="text" name="username" value={data.username} onChange={handleInputChange} />
-      <input type="password" name="password" value={data.password} onChange={handleInputChange} />
-      <input type="checkbox" name="remember" value={data.remember} onChange={handleInputChange} />
-    </form>
+      <form>
+        <input type="text" name="username" value={data.username} onChange={handleInputChange} />
+        <input type="password" name="password" value={data.password} onChange={handleInputChange} />
+        <input type="checkbox" name="remember" value={data.remember} onChange={handleInputChange} />
+        <button onClick={handleLogin} disabled={isButtonDisabled}>Login</button>
+      </form>
     </>
   )
 }
