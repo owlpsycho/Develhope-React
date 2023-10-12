02ac69fb1004f1fe66a6bde214958702b0e0d0f3
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-12 17:07:31 +0200

    risoluzione esercizio 43

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export function Login({ onLogin }) {
+export function Login({ event }) {
   const [data, setData] = useState({
     username: '',
     password: '',
