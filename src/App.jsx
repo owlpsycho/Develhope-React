7d39caa5ee63723887554dd0b8af10f815f71b3d
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-12 17:50:34 +0200

    risoluzione esercizio 46

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import { Login } from './Login'
 import './App.css'
-import { UncontrolledLogin } from './UncontrolledLogin';
+import { UncontrolledLogin } from './UncontrolledLogin'
 
 function App() {
 
-  const onLogin = (event, data) => {
-    event.preventDefault();
-    console.log('data', data)
-  }
-
   return (
     <>
-      <Login event={onLogin} />
-      {/* <UncontrolledLogin /> */}
+      <Login />
+      <UncontrolledLogin />
     </>
   )
 }
