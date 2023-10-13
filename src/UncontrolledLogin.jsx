7d0659572e565e214c3dab957b36782b365f5a70
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-13 09:19:32 +0200

    risoluzione esercizio 47

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -2,14 +2,25 @@ export function UncontrolledLogin (){
     function handleSubmit(event) {
         event.preventDefault()
 
-        const username = event.target.elements.namedItem("username").value
-        const password = event.target.elements.namedItem("password").value
-        const remember = event.target.elements.namedItem("remember").checked
+        // METODO 1
+        // const username = event.target.elements.namedItem("username").value
+        // const password = event.target.elements.namedItem("password").value
+        // const remember = event.target.elements.namedItem("remember").checked
+
+        // const data = {
+        //     username,
+        //     password,
+        //     remember
+        // }
+
+
+        // METODO 2
+        const formData = new formData(event.targer)
 
         const data = {
-            username,
-            password,
-            remember
+            username: formData.get("username"),
+            password: formData.get("password"),
+            remember: formData.get("remember") === "on" ? true : false
         }
 
         console.log(data);
