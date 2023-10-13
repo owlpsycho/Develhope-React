59bee310267956fb25d452f67a8f2873934f10d3
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-13 19:30:48 +0200

    risoluzione esercizio 50

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { FocusableInput } from './FocusableInput'
 
 function App() {
 
   return (
     <>
-      
+      <FocusableInput />
     </>
   )
 }
---
+++ src/FocusableInput.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from "react"
+
+export function FocusableInput(){
+    const _inputRef = useRef(null)
+
+    useEffect(()=>{
+        _inputRef.current?.focus()
+    }, [])
+
+    return (
+        <>
+            <input ref={_inputRef} type="text" />
+        </>
+    )
+}
