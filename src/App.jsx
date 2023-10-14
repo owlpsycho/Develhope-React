5535fe6e17eda9ac58a18d3c9c1a6f7c924d7477
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-14 14:31:15 +0200

    risoluzione esercizio 56

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import TodoList from './TodoList'
 
 function App() {
 
   return (
     <>
-      
+      <TodoList items={[]}/>
     </>
   )
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const TodoList = ({ items }) => {
+    const [toDoInput, setToDoInput] = useState("");
+    const [toDoList, setToDoList] = useState(items);
+
+    const handleInputChange = (event) => {
+        setToDoInput(event.target.value);
+    }
+
+    const handleAddToDo = () => {
+        setToDoList([
+            ...toDoList, 
+            toDoInput
+        ]);
+        setToDoInput("");
+    }
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            handleAddToDo();
+        }
+    }
+
+    return (
+        <>
+            <ul>
+                {toDoList.map((item) => <li>{item}</li>)}
+            </ul>
+            <input type="text" value={toDoInput} onChange={handleInputChange} onKeyPress={handleKeyPress} />
+            <button onClick={handleAddToDo}>Add</button>
+        </>
+    );
+}
+
+export default TodoList;
