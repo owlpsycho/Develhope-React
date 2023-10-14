62b6e1cd5f94dde704b1e10b916ec9b706eee4fb
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-14 14:41:26 +0200

    risoluzione esercizio 58

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -23,6 +23,10 @@ const TodoList = ({ items }) => {
         }
     }
 
+    const handleResetItemsArray = () => {
+        setToDoList([])
+    }
+
     return (
         <>
             <ul>
@@ -30,6 +34,7 @@ const TodoList = ({ items }) => {
             </ul>
             <input type="text" value={toDoInput} onChange={handleInputChange} onKeyPress={handleKeyPress} />
             <button onClick={handleAddToDo}>Add</button>
+            <button onClick={handleResetItemsArray}>Reset</button>
         </>
     );
 }
