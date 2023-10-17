028d102c2958a4b02bece040a0d926c1787612d7
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-17 18:50:24 +0200

    risoluzione esercizio 67

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import { Hello } from './Hello'
 import { Message } from './Message'
+import { Container } from './Container'
 
 function App() {
 
   return (
-    <>
+    <Container>
       <Hello />
       <Message />
-    </>
+    </Container>
   )
 }
 
