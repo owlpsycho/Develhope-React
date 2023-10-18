1fd43777c5c0f85aeb10c8195a1f9c9fe56545fc
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 14:28:39 +0200

    risoluzione esercizio 68

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,12 +1,10 @@
-import "./Container.css"
+import "./Container.css";
 
-export function Container({ title, children }){
-    
-
-    return (
-        <div className="container">
-            <div className="container-title">{title}</div>
-            <div className="container-content">{children}</div>
-        </div>
-    )
+export function Container({ title, children }) {
+  return (
+    <div className="container">
+      <div className="container-title">{title}</div>
+      <div className="container-content">{children}</div>
+    </div>
+  );
 }
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,3 +1,3 @@
-export function Hello () {
-    return <h2>Hello, World!</h2>
+export function Hello() {
+  return <h2>Hello, World!</h2>;
 }
