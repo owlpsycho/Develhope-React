f663cc14cf54c43bdf786626bd1d0953ede96727
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 14:29:11 +0200

    risoluzione esercizio 69

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import './App.css'
-import { Hello } from './Hello'
-import { Message } from './Message'
-import { Container } from './Container'
+import "./App.css";
+import { Hello } from "./Hello";
+import { Message } from "./Message";
+import { Container } from "./Container";
 
 function App() {
-
   return (
     <Container title={<Hello />}>
       <Message />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
