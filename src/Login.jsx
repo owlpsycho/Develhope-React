82cfa6f073d5e80ae3fff4586856299b8d90c684
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:57:44 +0200

    risoluzione esercizio 44

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,47 +2,67 @@ import React, { useState } from "react";
 
 export function Login() {
   const [data, setData] = useState({
-    username: '',
-    password: '',
-    remember: false
+    username: "",
+    password: "",
+    remember: false,
   });
 
   const handleInputChange = (event) => {
     const name = event.target.name;
-    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
 
-    setData(data => {
+    setData((data) => {
       return {
-        ...data, 
-        [name]: value
-      }
+        ...data,
+        [name]: value,
+      };
     });
-  }
+  };
 
-  const isButtonDisabled = data.username === '' || data.password === '';
+  const isButtonDisabled = data.username === "" || data.password === "";
 
   const handleLogin = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     onLogin(data);
   };
 
   const handleReset = () => {
     setData({
-      username: '',
-      password: '',
-      remember: false
+      username: "",
+      password: "",
+      remember: false,
     });
-  }
+  };
 
   return (
     <>
       <form>
-        <input type="text" name="username" value={data.username} onChange={handleInputChange} />
-        <input type="password" name="password" value={data.password} onChange={handleInputChange} />
-        <input type="checkbox" name="remember" checked={data.remember} onChange={handleInputChange} />
-        <button onClick={handleLogin} disabled={isButtonDisabled}>Login</button>
+        <input
+          type="text"
+          name="username"
+          value={data.username}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={data.password}
+          onChange={handleInputChange}
+        />
+        <input
+          type="checkbox"
+          name="remember"
+          checked={data.remember}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleLogin} disabled={isButtonDisabled}>
+          Login
+        </button>
         <button onClick={handleReset}>Reset</button>
       </form>
     </>
-  )
+  );
 }
