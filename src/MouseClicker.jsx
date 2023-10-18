906f8fc43d3145422cd247564f8ab937c8513752
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:54:00 +0200

    risoluzione esercizio 38

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,19 +1,22 @@
 import faviconImage from "./assets/favicon-16x16.png";
 
 export const MouseClicker = ({ name }) => {
-    const handleClicker = (event) => {
-        console.log(event.currentTarget.name)
-    }
+  const handleClicker = (event) => {
+    console.log(event.currentTarget.name);
+  };
 
-    const handleImgClicker = (event) => {
-        console.log(event.currentTarget.src)
-    }
+  const handleImgClicker = (event) => {
+    console.log(event.currentTarget.src);
+  };
 
-        //vengono eseguiti insieme a causa dell'effetto bubble di html dato che l'immagine si trova all'interno del bottone
+  //vengono eseguiti insieme a causa dell'effetto bubble di html dato che l'immagine si trova all'interno del bottone
 
-    return (
-        <>
-            <button name={name} onClick={handleClicker}><img src={faviconImage} onClick={handleImgClicker}/>Click Me!</button>
-        </>
-    )
-}
+  return (
+    <>
+      <button name={name} onClick={handleClicker}>
+        <img src={faviconImage} onClick={handleImgClicker} />
+        Click Me!
+      </button>
+    </>
+  );
+};
