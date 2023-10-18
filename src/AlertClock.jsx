361e4eec0f4e26d09f85ddfd1e290e26f5572e96
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 12:35:45 +0200

    correzione esercizio 27

--- src/AlertClock.jsx
+++ src/AlertClock.jsx
@@ -1,11 +1,11 @@
 export function AlertClock() {
-    function handleButtonClick() {
-        alert('Current time: ' + new Date().toLocaleTimeString())
-        }
-        return (
-            <div>
-                <p>Click the button to see the current time.</p>
-                <button onClick={handleButtonClick}>Click Me!</button>
-            </div>
-        )
+  function handleButtonClick() {
+    alert("Current time: " + new Date().toLocaleTimeString());
+  }
+  return (
+    <>
+      <p>Click the button to see the current time.</p>
+      <button onClick={handleButtonClick}>Click Me!</button>
+    </>
+  );
 }
