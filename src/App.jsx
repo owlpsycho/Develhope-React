34bf45f26f03418131027430b39a966b8711ae94
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:59:09 +0200

    risoluzione esercizio 47

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { Login } from './Login'
-import './App.css'
-import { UncontrolledLogin } from './UncontrolledLogin'
+import { Login } from "./Login";
+import "./App.css";
+import { UncontrolledLogin } from "./UncontrolledLogin";
 
 function App() {
-
   return (
     <>
       <Login />
       <UncontrolledLogin />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
