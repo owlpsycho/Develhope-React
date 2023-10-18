1c6dabc0f094699f948d1753cae155823009a079
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 11:49:03 +0200

    correzione esercizio 21

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import { Welcome } from './Welcome'
+import { Age } from './Age'
 
 function App() {
 
   return (
     <>
-      <Welcome name="Denis" age={17} />
+      <Welcome name="Denis" />
+      <Age age={21} />
     </>
   )
 }
