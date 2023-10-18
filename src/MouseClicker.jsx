64b304a19c574a366089466374271f2bb3147f6a
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:53:37 +0200

    risoluzione esercizio 37

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,15 +1,15 @@
 import faviconImage from "./assets/favicon-16x16.png";
 
 export const MouseClicker = ({ name }) => {
-    const handleClicker = (event) => {
-        console.log(event.currentTarget.name)
-    }
+  const handleClicker = (event) => {
+    console.log(event.currentTarget.name);
+  };
 
-
-
-    return (
-        <>
-            <button name={name} onClick={handleClicker}><img src={faviconImage}/> Click Me!</button>
-        </>
-    )
-}
+  return (
+    <>
+      <button name={name} onClick={handleClicker}>
+        <img src={faviconImage} /> Click Me!
+      </button>
+    </>
+  );
+};
