e8470a67bb0b1d44d266924fdc6b2586dd1092b7
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 14:26:12 +0200

    risoluzione esercizio 63

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import "./Clock.scss"
+import "./Clock.scss";
 
 export const Clock = () => {
   const [time, setTime] = useState("");
