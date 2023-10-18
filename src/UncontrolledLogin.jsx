1390808b23938a0af03b0b2b717ead70f44088a6
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:58:45 +0200

    risoluzione esercizio 46

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -1,31 +1,29 @@
-export function UncontrolledLogin (){
-    function handleSubmit(event) {
-        event.preventDefault()
+export function UncontrolledLogin() {
+  function handleSubmit(event) {
+    event.preventDefault();
 
-        const username = event.target.elements.namedItem("username").value
-        const password = event.target.elements.namedItem("password").value
-        const remember = event.target.elements.namedItem("remember").checked
+    const username = event.target.elements.namedItem("username").value;
+    const password = event.target.elements.namedItem("password").value;
+    const remember = event.target.elements.namedItem("remember").checked;
 
-        const data = {
-            username,
-            password,
-            remember
-        }
+    const data = {
+      username,
+      password,
+      remember,
+    };
 
-        console.log(data);
+    console.log(data);
+  }
 
-    }
-
-
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input name="username" />
-                <input name="password" type="password"/>
-                <input name="remember" type="checkbox" />
-                <button>Login</button>
-                <button type="reset">Reset</button>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input name="username" />
+        <input name="password" type="password" />
+        <input name="remember" type="checkbox" />
+        <button>Login</button>
+        <button type="reset">Reset</button>
+      </form>
+    </>
+  );
 }
