5266bf86660db8cfc4b9207f176779cb72f53dc6
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:55:12 +0200

    risoluzione esercizio 40

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import { MultiButton } from './MultiButton'
+import { useState } from "react";
+import "./App.css";
+import { MultiButton } from "./MultiButton";
 
 function App() {
-
   return (
     <>
       <MultiButton btn1="ONE" btn2="TWO" btn3="THREE" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
