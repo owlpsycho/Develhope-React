50263b6a13e095fffb6f22c33daadb42155bdef1
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:52:41 +0200

    risoluzione esercizio 35

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import { Clock } from './Clock'
+import { useState } from "react";
+import "./App.css";
+import { Clock } from "./Clock";
 
 function App() {
-
   return (
     <>
       <Clock />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
