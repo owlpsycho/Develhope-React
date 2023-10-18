455fef7f7fb40eca31b8b7d8570b65cfa796d0d3
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 14:22:44 +0200

    risoluzione esercizio 59

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,50 +1,52 @@
 import React, { useState } from "react";
 
 const TodoList = ({ items }) => {
-    const [toDoInput, setToDoInput] = useState("");
-    const [toDoList, setToDoList] = useState(items);
+  const [toDoInput, setToDoInput] = useState("");
+  const [toDoList, setToDoList] = useState(items);
 
-    const handleInputChange = (event) => {
-        setToDoInput(event.target.value);
+  const handleInputChange = (event) => {
+    setToDoInput(event.target.value);
+  };
+
+  const handleAddToDo = () => {
+    setToDoList([...toDoList, toDoInput]);
+    setToDoInput("");
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleAddToDo();
     }
+  };
 
-    const handleAddToDo = () => {
-        setToDoList([
-            ...toDoList,
-            toDoInput
-        ]);
-        setToDoInput("");
-    }
+  const handleResetItemsArray = () => {
+    setToDoList([]);
+  };
 
-    const handleKeyPress = (e) => {
-        if (e.key === "Enter") {
-            handleAddToDo();
-        }
-    }
+  const handleRemoveItems = (index) => {
+    setToDoList(toDoList.filter((element, i) => i !== index));
+  };
 
-    const handleResetItemsArray = () => {
-        setToDoList([]);
-    }
-
-    const handleRemoveItems = (index) => {  
-        setToDoList(toDoList.filter((element, i) => i !== index));
-    }
-
-    return (
-        <>
-            <ul>
-                {toDoList.map((item, index) => (
-                    <li key={index}>
-                        {item}
-                        <button onClick={() => handleRemoveItems(index)}>Delete</button>
-                    </li>
-                ))}
-            </ul>
-            <input type="text" value={toDoInput} onChange={handleInputChange} onKeyPress={handleKeyPress} />
-            <button onClick={handleAddToDo}>Add</button>
-            <button onClick={handleResetItemsArray}>Reset</button>
-        </>
-    );
-}
+  return (
+    <>
+      <ul>
+        {toDoList.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => handleRemoveItems(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={toDoInput}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={handleAddToDo}>Add</button>
+      <button onClick={handleResetItemsArray}>Reset</button>
+    </>
+  );
+};
 
 export default TodoList;
