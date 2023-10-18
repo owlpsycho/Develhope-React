1bd43c194644b5ea7f342abac4648bc6be196011
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:49:35 +0200

    risoluzione esercizio 69

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,22 +1,24 @@
-import { useState } from "react"
-import "./Container.css"
+import { useState } from "react";
+import "./Container.css";
 
-export function Container({ title, children }){
-    const [collapsed, setCollapsed] = useState(true)
-    
-    const handleToggle = () => {
-        setCollapsed(t => !t)
-    }
+export function Container({ title, children }) {
+  const [collapsed, setCollapsed] = useState(true);
 
-    return (
-        <div className="container">
-            <div className="container-btn"><button onClick={handleToggle}>Toggle</button></div>
-            {collapsed && (
-                <>
-                    <div className="container-title">{title}</div> 
-                    <div className="container-content">{children}</div>
-                </>    
-            )}
-        </div>
-    )
+  const handleToggle = () => {
+    setCollapsed((t) => !t);
+  };
+
+  return (
+    <div className="container">
+      <div className="container-btn">
+        <button onClick={handleToggle}>Toggle</button>
+      </div>
+      {collapsed && (
+        <>
+          <div className="container-title">{title}</div>
+          <div className="container-content">{children}</div>
+        </>
+      )}
+    </div>
+  );
 }
