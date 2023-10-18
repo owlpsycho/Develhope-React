a5fcf5ac3a0fe8e56b4c1f2f992bbbcb903ea921
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:50:33 +0200

    risoluzione esercizio 31

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,16 +1,15 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export function Counter({ startValue, increment}){
-    const [count, setCount] = useState(startValue)
-    function handleClick (){
-        setCount(count + increment)
-    }
+export function Counter({ startValue, increment }) {
+  const [count, setCount] = useState(startValue);
+  function handleClick() {
+    setCount(count + increment);
+  }
 
-    return (
-        <>
-            <h2>{count}</h2>
-            <button onClick={handleClick}>Click Me!</button>
-        </>
-    )
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={handleClick}>Click Me!</button>
+    </>
+  );
 }
-
