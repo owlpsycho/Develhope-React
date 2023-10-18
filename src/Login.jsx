e2e57d05008076e61b078a92d87d91083b0ad100
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:56:20 +0200

    risoluzione esercizio 42

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,32 +2,48 @@ import { useState } from "react";
 
 export function Login() {
   const [data, setData] = useState({
-    username: '',
-    password: '',
-    remember: false
+    username: "",
+    password: "",
+    remember: false,
   });
 
   const handleInputChange = (event) => {
     const name = event.target.name;
-    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
 
-
-    setData(data => {
+    setData((data) => {
       return {
-        ...data, 
-        [name]: value
-        
-      }
-    })
-}
+        ...data,
+        [name]: value,
+      };
+    });
+  };
 
   return (
     <>
-    <form>
-      <input type="text" name="username" value={data.username} onChange={handleInputChange} />
-      <input type="password" name="password" value={data.password} onChange={handleInputChange} />
-      <input type="checkbox" name="remember" checked={data.remember} onChange={handleInputChange} />
-    </form>
+      <form>
+        <input
+          type="text"
+          name="username"
+          value={data.username}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={data.password}
+          onChange={handleInputChange}
+        />
+        <input
+          type="checkbox"
+          name="remember"
+          checked={data.remember}
+          onChange={handleInputChange}
+        />
+      </form>
     </>
-  )
+  );
 }
