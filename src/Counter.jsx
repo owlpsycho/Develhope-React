1d02e7a1a1411e2056c82403be0eb3550ada4b16
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 13:52:00 +0200

    risoluzione esercizio 34

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,33 +1,32 @@
-import { useState } from "react"
-import { Button } from "./Button"
-import { useEffect } from "react"
+import { useState } from "react";
+import { Button } from "./Button";
+import { useEffect } from "react";
 
-export function Counter({ startValue, increment}){
-    const [count, setCount] = useState(startValue)
+export function Counter({ startValue, increment }) {
+  const [count, setCount] = useState(startValue);
 
-    function handleClickPlus (){
-        setCount(count + increment)
-    }
+  function handleClickPlus() {
+    setCount(count + increment);
+  }
 
-    function handleClickMinus (){
-        setCount(count - increment)
-    }
+  function handleClickMinus() {
+    setCount(count - increment);
+  }
 
-    function handleClickReset (){
-        setCount(0)
-    }
+  function handleClickReset() {
+    setCount(0);
+  }
 
-    useEffect(()=>{
-        console.log(count)
-    },[count])
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
 
-    return (
-        <>
-            <h2>{count}</h2>
-            <Button name="Add" event={handleClickPlus}/>
-            <Button name="Subtract" event={handleClickMinus}/>
-            <Button name="Reset" event={handleClickReset}/>
-        </>
-    )
+  return (
+    <>
+      <h2>{count}</h2>
+      <Button name="Add" event={handleClickPlus} />
+      <Button name="Subtract" event={handleClickMinus} />
+      <Button name="Reset" event={handleClickReset} />
+    </>
+  );
 }
-
