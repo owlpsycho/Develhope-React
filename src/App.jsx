b4e95dc0c355f2d314936680dd5bd8f4fd1f7775
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-18 14:27:25 +0200

    risoluzione esercizio 64

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
-import TodoList from './TodoList'
+import "./App.css";
+import TodoList from "./TodoList";
 
 function App() {
-
   return (
     <>
-      <TodoList items={["Prova 1", "Prova 2"]}/>
+      <TodoList items={["Prova 1", "Prova 2"]} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
