cfe23a05633f023475287d8a44b7a6f5d0f348d2
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-19 16:39:17 +0200

    risoluzione esercizio 71

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
+import { LanguageContext } from "./LanguageContext";
 
 export const Clock = () => {
   const [time, setTime] = useState("");
+  const language = useContext(LanguageContext);
 
   function currentTime() {
     const time = new Date().toLocaleTimeString();
@@ -20,7 +22,7 @@ export const Clock = () => {
 
   return (
     <>
-      <h1>Sono le</h1>
+      {language === "it" ? <h2>Sono le</h2> : <h2>It is</h2>}
       <h1>{time}</h1>
     </>
   );
