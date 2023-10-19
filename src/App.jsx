34a3570abce6707a326fdfc5d18efee0493d5b56
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-19 16:50:50 +0200

    risoluzione esercizio 72

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,11 @@ function App() {
         <option value="it">IT</option>
         <option value="en">EN</option>
       </select>
-      <LanguageContext.Provider value={language}>
-        <Clock />
-      </LanguageContext.Provider>
+      <Clock />
+      {/* il Contex Provider è responsabile di rendere disponibile il value "language" a tutti i componenti figli all'interno del suo ambito. 
+      Senza il provider, il componente Clock non avrà accesso a questo valore e perderà la sua capacità di rispondere alle modifiche della lingua selezionata */}
     </div>
   );
 }
 
 export default App;
-
