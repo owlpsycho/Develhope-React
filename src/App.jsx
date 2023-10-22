d97824b4b72fc1468a6f65de7166a871c256333a
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-22 17:48:15 +0200

    risoluzione esercizio 81

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import FilteredList from "./FilteredList";
 
 function App() {
-
   return (
     <>
-      
+      <FilteredList
+        list={[
+          {
+            name: "Denis",
+            id: 28,
+            age: 21,
+          },
+          {
+            name: "Nicola",
+            id: 20,
+            age: 27,
+          },
+          {
+            name: "Andrea",
+            id: 1,
+            age: 17,
+          },
+        ]}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/FilteredList.jsx
@@ -0,0 +1,22 @@
+import React, { useMemo } from "react";
+
+function FilteredList({ list }) {
+  const filteredList = useMemo(() => {
+    return list.filter((item) => item.age >= 18);
+  }, [list]);
+
+  return (
+    <>
+      <h1>Filtered List</h1>
+      <ul>
+        {filteredList.map((item) => (
+          <li key={item.id}>
+            {item.name} (Age: {item.age})
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default FilteredList;
