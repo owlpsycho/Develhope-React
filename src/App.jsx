d4c08cac7adcdb8286fcdf2726b52810664750b4
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-22 19:10:26 +0200

    risoluzione esercizio 65

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { Login } from "./Login";
 import "./App.css";
 import { UncontrolledLogin } from "./UncontrolledLogin";
 
 function App() {
   return (
     <>
-      <Login />
       <UncontrolledLogin />
     </>
   );
--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -1,25 +1,6 @@
 export function UncontrolledLogin() {
   function handleSubmit(event) {
     event.preventDefault();
-
-    //METODO 1
-    //(sintassi standard)
-    // const username = event.target.elements.namedItem("username").value
-    // const password = event.target.elements.namedItem("password").value
-    // const remember = event.target.elements.namedItem("remember").checked
-
-    //(altra sintassi)
-    // const username = event.target.username.value
-    // const password = event.target.password.value
-    // const remember = event.target.remember.checked
-
-    // const data = {
-    //     username,
-    //     password,
-    //     remember
-    // }
-
-    // METODO 2
     const formData = new FormData(event.target);
 
     const data = {
@@ -27,20 +8,55 @@ export function UncontrolledLogin() {
       password: formData.get("password"),
       remember: formData.get("remember") === "on" ? true : false,
     };
-    //VANTAGGI: È facile da usare, gestisce dati di form piû complessi e supportata da tutti i principali browser attuali
-    //SVANTAGGI: Non supportato da browser più vecchi ed è più difficile eseguire il debug
 
     console.log(data);
+    event.target.reset();
   }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input name="username" />
-        <input name="password" type="password" />
-        <input name="remember" type="checkbox" />
-        <button>Login</button>
-        <button type="reset">Reset</button>
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <input
+            name="username"
+            className="relative bg-gray-50ring-0 outline-none border border-neutral-500 text-white placeholder-violet-700 text-sm rounded-lg focus:ring-violet-500  focus:border-violet-500 block w-64 p-2.5 checked:bg-emerald-500"
+          />
+          <input
+            name="password"
+            type="password"
+            className="block w-full px-6 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm max-w-[220px]"
+            placeholder="******"
+          />
+        </div>
+        <div className="flex mt-5 gap-3">
+        <label class="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" name="remember" class="sr-only peer" />
+          <div className="peer ring-0 bg-rose-400  rounded-full outline-none duration-300 after:duration-500 w-12 h-12  shadow-md peer-checked:bg-emerald-500  peer-focus:outline-none  after:content-['✖️'] after:rounded-full after:absolute after:outline-none after:h-10 after:w-10 after:bg-gray-50 after:top-1 after:left-1 after:flex after:justify-center after:items-center  peer-hover:after:scale-75 peer-checked:after:content-['✔️'] after:-rotate-180 peer-checked:after:rotate-0"></div>
+        </label>
+        <button className="relative py-2 px-8 text-black text-base font-bold uppercase rounded-[50px] overflow-hidden bg-white transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-purple-500 before:to-red-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-[50px] hover:before:left-0">
+          Login
+        </button>
+        <button
+          type="reset"
+          className="inline-flex items-center px-4 py-2 bg-red-600 transition ease-in-out delay-75 hover:bg-red-700 text-white text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110"
+        >
+          <svg
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            fill="none"
+            class="h-5 w-5 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              stroke-width="2"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+          Reset
+        </button>
+        </div>
       </form>
     </>
   );
