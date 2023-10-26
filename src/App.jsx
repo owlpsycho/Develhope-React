c0af9107386e1ceb55973dd6b0957e862266fe29
Denis Bruno <denisbruno.DB28@gmail.com>
2023-10-26 10:56:29 +0200

    risoluzione esercizio 82

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 
 function App() {
   return (
     <>
-      <Welcome name="Denis" />
+      <Routes>
+        <Route path="/" element={<Welcome name="Denis" />} />
+      </Routes>
     </>
   );
 }
