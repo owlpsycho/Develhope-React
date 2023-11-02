66ce0e0bf8680ac702955532446a8a28c2916901
Denis Bruno <denisbruno.DB28@gmail.com>
2023-11-02 11:12:25 +0100

    risoluzione esercizio 90

--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -2,8 +2,12 @@ import useSWR from "swr";
 
 export function useGithubUser(username) {
   const fetcher = (url) => fetch(url).then(res => res.json());
-  const { data, error, isLoading } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+  
+  if (username) {
+    const { data, error, isLoading } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    return { data, error, isLoading }
+  }
 
-  return {data, error, isLoading}
+  return { data: null, error: null, isLoading: false }
 }
 
