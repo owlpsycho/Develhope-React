7b4e8cebb14f248b5b304941a9ae1da0e1f2f69d
Denis Bruno <denisbruno.DB28@gmail.com>
2023-11-02 11:09:57 +0100

    risoluzione esercizio 89

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -1,10 +1,12 @@
 import { useGithubUser } from "./useGithubUser";
 
 export const GithubUser = ({username}) => {
-  const { data } = useGithubUser(username);
+  const { data, error, isLoading } = useGithubUser(username);
 
   return (
     <>
+      {isLoading && <h1>Loading...</h1>}
+      {error && <h1>{error.message}</h1>}
       <h1>{data?.login}</h1>
       <h2>{data?.name}</h2>
       <img src={data?.avatar_url} />
--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -1,26 +1,9 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import useSWR from "swr";
 
 export function useGithubUser(username) {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
+  const fetcher = (url) => fetch(url).then(res => res.json());
+  const { data, error, isLoading } = useSWR(`https://api.github.com/users/${username}`, fetcher)
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      if (!response.ok) {
-        throw new Error("Errore nella richiesta");
-      }
-      const dataJSON = await response.json();
-      setData(dataJSON);
-    } catch (error) {
-      setError(error);
-    }
-  };
-
-  useEffect(() => {
-      fetchData();
-  }, []);
-
-  return {data}
+  return {data, error, isLoading}
 }
+
